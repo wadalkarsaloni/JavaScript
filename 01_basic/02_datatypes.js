8e84d490f4d4f6b2c617a29465ff07263b53e007
saloni wadalkar <127375584+wadalkarsaloni@users.noreply.github.com>
2024-03-15 22:06:39 +0000

    datatypes

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,20 @@
+"use Strict"; //treat all js code as newer version
+
+//alert("hello");  //if u run this on terminal it shows error because we r using nodejs not a browser
+
+
+let name = "saloni"
+let age =18
+let isLoggedIn = false
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null(it is a standalone value) representation of empty value 
+//undefined => when the value is not defined\
+//symbol => unique
+
+//object
+
+console.log(typeof "saloni")
