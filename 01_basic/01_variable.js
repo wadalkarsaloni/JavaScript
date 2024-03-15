babb3465779fb19e933a29a0ddc22b82fa7b912f
saloni wadalkar <127375584+wadalkarsaloni@users.noreply.github.com>
2024-03-15 22:08:23 +0000

    variables

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,6 @@
+const accountId = 123456
+let accountEmail = "saloniwadalkar@gmail.com"
+var accountPassword = "123987"
+accountCity = "Pune"
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);
